549697d9a09ba1c079ad779166b9362570c5f51c
Tang <taksin_tang51@hotmail.com>
2025-11-09 11:23:10 +0700

    [Update] set multiplier score

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,5 @@
 using System;
 using UnityEngine;
-using UnityEngine.SocialPlatforms.Impl;
 
 public class GameManager : MonoBehaviour
 {
@@ -9,6 +8,7 @@ public class GameManager : MonoBehaviour
     public static Action<int> OnScoreUpdate;
 
     public static GameManager instance;
+    private float currentMultiplier = 1f;
 
     private void Awake()
     {
@@ -26,7 +26,22 @@ public class GameManager : MonoBehaviour
 
     public void AddScore(int amount)
     {
-        score += amount;
+        int scoreToAdd = (int)(amount * currentMultiplier);
+        score += scoreToAdd;
         OnScoreUpdate?.Invoke(score);
+    }
+
+    public void SetMultiplier(float multiplier)
+    {
+        // Logic to set score multiplier
+        currentMultiplier = multiplier;
+        Debug.Log("Multiplier set to: " + currentMultiplier + "x");
+    }
+
+    public void ResetScore()
+    {
+        score = 0;
+        OnScoreUpdate?.Invoke(score);
+        Debug.Log("Score reset to zero.");
     }
 }
